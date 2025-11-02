679d841c07d3d2f5b80331c1fcf54397ed70b539
pharuxtan <28842091+pharuxtan@users.noreply.github.com>
2025-11-02 10:41:10 +0100

    feat(recipe): add blaze rod to mob tears recipe back

--- src/main/java/wootrevived/woot/datagen/recipes/FluidInfuser.java
+++ src/main/java/wootrevived/woot/datagen/recipes/FluidInfuser.java
@@ -103,7 +103,8 @@ public class FluidInfuser {
                         Items.SLIME_BALL
                 ), 1000),
                 new IngredientRecipe(Ingredient.of(
-                        Items.MAGMA_CREAM
+                        Items.MAGMA_CREAM,
+                        Items.BLAZE_ROD
                 ), 1500),
                 new IngredientRecipe(Ingredient.of(
                         Items.ENDER_PEARL,
