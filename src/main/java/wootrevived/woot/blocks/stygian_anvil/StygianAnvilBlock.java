d5c0192c90abb047a3462d3fa70d10189a57f8ed
pharuxtan <28842091+pharuxtan@users.noreply.github.com>
2025-11-29 16:46:41 +0100

    feat(magmator): add magmator to automate anvils

--- src/main/java/wootrevived/woot/blocks/stygian_anvil/StygianAnvilBlock.java
+++ src/main/java/wootrevived/woot/blocks/stygian_anvil/StygianAnvilBlock.java
@@ -12,6 +12,7 @@ import net.minecraft.server.level.ParticleStatus;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
@@ -79,7 +80,13 @@ public class StygianAnvilBlock extends Block implements EntityBlock {
     }
 
     public boolean isAnvilHot(Level level, BlockPos pos) {
-        return level.getBlockState(pos.below()).getBlock() == Blocks.MAGMA_BLOCK;
+        Block block = level.getBlockState(pos.below()).getBlock();
+        return block == Blocks.MAGMA_BLOCK ||
+                block == BlocksRegistry.COPPER_MAGMATOR_BLOCK.get() ||
+                block == BlocksRegistry.IRON_MAGMATOR_BLOCK.get() ||
+                block == BlocksRegistry.GOLD_MAGMATOR_BLOCK.get() ||
+                block == BlocksRegistry.DIAMOND_MAGMATOR_BLOCK.get() ||
+                block == BlocksRegistry.NETHERITE_MAGMATOR_BLOCK.get();
     }
 
     @OnlyIn(Dist.CLIENT)
@@ -103,7 +110,7 @@ public class StygianAnvilBlock extends Block implements EntityBlock {
         @Override
         public InteractionResult useItemOn(@NotNull ItemStack heldItem, @NotNull Level level, @NotNull Player player, @NotNull InteractionHand hand, @NotNull BlockHitResult hit) {
             if (level.isClientSide)
-                super.useItemOn(heldItem, level, player, hand, hit);
+                return InteractionResult.SUCCESS;
 
             BlockEntity be = level.getBlockEntity(hit.getBlockPos());
             if (be instanceof StygianAnvilBlockEntity anvil) {
@@ -111,7 +118,15 @@ public class StygianAnvilBlock extends Block implements EntityBlock {
                     anvil.dropItem(player, hand);
                 } else if (heldItem.getItem() == ItemsRegistry.STYGIAN_HAMMER_ITEM.get()) {
                     // Crafting
-                    anvil.tryCraft(player);
+                    ItemStack stack = anvil.tryCraft(player, false);
+                    if(!stack.isEmpty()){
+                        BlockPos anvilPos = anvil.getBlockPos();
+                        ItemEntity itemEntity = new ItemEntity(level,
+                                anvilPos.getX(), anvilPos.getY() + 1, anvilPos.getZ(),
+                                stack);
+                        itemEntity.setDefaultPickUpDelay();
+                        level.addFreshEntity(itemEntity);
+                    }
                 } else {
                     IItemHandler itemHandler = anvil.getInventory();
                     for(int slot = 0; slot < itemHandler.getSlots(); slot++) {
--- src/main/java/wootrevived/woot/datagen/LootTables.java
+++ src/main/java/wootrevived/woot/datagen/LootTables.java
@@ -43,6 +43,12 @@ public class LootTables extends BlockLootSubProvider {
 
         copyComponentWithEnable(BlocksRegistry.FAKE_SPAWNER_BLOCK, ComponentsRegistry.FAKE_SPAWNER_DATA);
 
+        dropSelf(BlocksRegistry.COPPER_MAGMATOR_BLOCK);
+        dropSelf(BlocksRegistry.IRON_MAGMATOR_BLOCK);
+        dropSelf(BlocksRegistry.GOLD_MAGMATOR_BLOCK);
+        dropSelf(BlocksRegistry.DIAMOND_MAGMATOR_BLOCK);
+        dropSelf(BlocksRegistry.NETHERITE_MAGMATOR_BLOCK);
+
         dropSelfWithEnable(BlocksRegistry.COPPER_PLINTH_BLOCK);
         dropSelfWithEnable(BlocksRegistry.COPPER_PYLON_BLOCK);
         copyComponentWithEnable(BlocksRegistry.COPPER_CELL_BLOCK, ComponentsRegistry.CELL_DATA);
