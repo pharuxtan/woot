69f642b1aa08c8ec02809c26593800adcb44f588
pharuxtan <28842091+pharuxtan@users.noreply.github.com>
2026-01-11 16:51:09 +0100

    feat(mass_upgrade): add config to duplicate items instead of rerolling the loot table

--- src/main/java/wootrevived/woot/upgrades/Mass.java
+++ src/main/java/wootrevived/woot/upgrades/Mass.java
@@ -16,14 +16,17 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.component.TooltipDisplay;
 import net.minecraft.world.item.component.TooltipProvider;
 import net.neoforged.neoforge.common.MutableDataComponentHolder;
+import net.neoforged.neoforge.fluids.FluidStack;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import org.jetbrains.annotations.NotNull;
 import wootrevived.api.WootUpgradeItem;
 import wootrevived.api.enums.UpgradeDefaultVariant;
+import wootrevived.api.interfaces.WootDropsProperties;
 import wootrevived.api.interfaces.WootGenerationProperties;
 import wootrevived.api.registrations.WootUpgradeItemRegistration;
 import wootrevived.woot.Woot;
+import wootrevived.woot.config.UpgradesConfig;
 import wootrevived.woot.registries.ComponentsRegistry;
 
 import java.util.function.Consumer;
@@ -40,7 +43,21 @@ public class Mass extends WootUpgradeItem<UpgradeDefaultVariant> {
 
     @Override
     public void applyGenerationProperties(@NotNull WootGenerationProperties properties, @NotNull MutableDataComponentHolder dataComponentHolder) {
-        properties.setNumberOfSimulations(2 * getVariant(dataComponentHolder).level());
+        if(UpgradesConfig.MASS_REROLL_LOOT.get())
+            properties.setNumberOfSimulations(2 * getVariant(dataComponentHolder).level());
+    }
+
+    @Override
+    public void modifyDrops(@NotNull WootDropsProperties properties, @NotNull MutableDataComponentHolder dataComponentHolder) {
+        if(!UpgradesConfig.MASS_REROLL_LOOT.get()) {
+            int multiplier = 2 * getVariant(dataComponentHolder).level();
+
+            for(ItemStack stack : properties.getItemDrops())
+                stack.setCount(stack.getCount() * multiplier);
+
+            for(FluidStack stack : properties.getFluidDrops())
+                stack.setAmount(stack.getAmount() * multiplier);
+        }
     }
 
     /* Upgrade Item registration */
